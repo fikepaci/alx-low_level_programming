19f87cc9776e972fa2ab72bbee552cec1a8e1d17
fikepaci <pacifiquemachine@gmail.com>
2020-09-16 14:29:52 +0200

    change 4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 
-int main()
+/**
+ * main - entry point of any C program
+ *
+ * Return:  specify a value to be returned
+ */
+int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle");
-    return 0;
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
