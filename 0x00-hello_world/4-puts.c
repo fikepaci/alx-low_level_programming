cd97e9e192b1da18984167ba8da9ff3ecd2da145
fikepaci <pacifiquemachine@gmail.com>
2020-09-16 14:17:13 +0200

    chnage 4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,7 @@
-#include <stdio.h> /*library command puts is stored in */
+#include <stdio.h>
 
 int main()
-
 {
-  puts ("Programming is like building a multilingual puzzle");
-
-  return 0; /* if code fails returns 0 */
+    puts(""Programming is like building a multilingual puzzle \n");
+    return(0);
 }
-/*curly braces contains arguements */
-/* " " are used to contain statement */
-/* ; are used to end statement */
