fc4171d0876a38fa6fda11e311a3d5a6a0ed321b
fikepaci <pacifiquemachine@gmail.com>
2020-09-16 15:15:38 +0200

    change 5-printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 
+/**
+ * main - entry point of any C program
+ *
+ * Return:  specify a value to be returned
+ */
 int main(void)
 {
-    printf ("\"with proper grammar, but the outcome is a piece of art, \n");
-    return 0;
+	printf("%s", "with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
