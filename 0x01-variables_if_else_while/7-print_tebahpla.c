0c12f54e29085ce8219c05a10fe28b3e74e7fe4c
fikepaci <pacifiquemachine@gmail.com>
2020-09-17 11:21:29 +0200

    add 7-print_tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	char letter;
+	
+	for (letter='z'; letter>='a'; letter--)
+	{
+		putchar(letter);
+	}
+    putchar('\n');
+	return (0);
+}
