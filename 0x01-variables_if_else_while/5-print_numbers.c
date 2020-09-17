6dcba5a73b597cdf3f83a7c6e35cfdbe2e7909cb
fikepaci <pacifiquemachine@gmail.com>
2020-09-17 11:05:53 +0200

    add 5-print_numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int a;
+	
+	for (a=0; a<10; a++)
+	{
+		printf("%d",a);
+	}
+	printf("\n");
+	return (0);
+}
