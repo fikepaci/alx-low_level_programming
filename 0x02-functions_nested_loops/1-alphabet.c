e20658a10c05de555df9ff07c81b23a1f59ba78a
fikepaci <pacifique@DESKTOP-F6F2EC6.localdomain>
2020-09-22 11:07:53 +0200

    add 1-alphabet.c file

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+*print_alphabet - print the alphabet
+*/
+void print_alphabet(void)
+{
+int letter;
+for (letter = 'a'; letter <= 'z'; letter++)
+{
+_putchar(letter);
+}
+_putchar('\n');
+}
