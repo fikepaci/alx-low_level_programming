8bb48881234e25b995a11fb389fd1e61b23b0325
fikepaci <pacifiquemachine@gmail.com>
2020-09-22 13:34:09 +0200

    change 0-holberton file

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,28 +1,19 @@
-/*
- * File: 0-holberton.c
- * Auth: Brennan D Baraban
- *
- */
-
 #include "holberton.h"
-
 /**
- * main - Prints "Holberton" followed by a new line.
+ * main - check the code for Holberton School students.
  *
  * Return: Always 0.
  */
+
 int main(void)
 {
-	_putchar('H');
-	_putchar('o');
-	_putchar('l');
-	_putchar('b');
-	_putchar('e');
-	_putchar('r');
-	_putchar('t');
-	_putchar('o');
-	_putchar('n');
-	_putchar('\n');
+	char holberton[9] = "Holberton";
+	int i;
 
+	for (i = 0; i < 9; i++)
+	{
+		_putchar(holberton[i]);
+	}
+	_putchar('\n');
 	return (0);
 }
