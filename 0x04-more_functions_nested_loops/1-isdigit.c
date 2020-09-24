55c020714133c5e41e9518ee3fe776bc078ec5c5
fikepaci <pacifiquemachine@gmail.com>
2020-09-24 11:06:21 +0200

    add _isdigit file

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+*_isdigit - Checks for a difit (0-9)
+*@c: the number to be checked.
+*
+*Return: 1 if the number is a digit, 0 otherwise.
+*/
+int _isdigit(int c)
+{
+if (c >= '0' && c <= '9')
+return (1);
+else
+return (0);
+}
