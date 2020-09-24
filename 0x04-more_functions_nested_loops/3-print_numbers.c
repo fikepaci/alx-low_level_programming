71b90096146db6bf99c002ae8d33d1c5b97422cc
fikepaci <pacifiquemachine@gmail.com>
2020-09-24 11:20:12 +0200

    add 3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+* print_numbers - Prints the numbers, from 0 to 9, followed by a new line
+*/
+void print_numbers(void)
+{
+int a;
+for (a = 0; a <= 9; a++)
+_putchar((a % 10) + '0');
+_putchar('\n');
+}
