29c8c8fd5c1dec52e23b9abd0374179e4eab7ec6
fikepaci <pacifiquemachine@gmail.com>
2020-09-24 11:12:36 +0200

    add 2-mul.c file

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+* mul - Multipies two integers
+*@a: first integer to be multiplied.
+*@b: secnd integer to be multiplied.
+*
+*Return: the result of multiplication.
+*/
+int mul(int a, int b)
+{
+return (a * b);
+}
