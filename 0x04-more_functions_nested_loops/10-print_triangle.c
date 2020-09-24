eab1f24962af8e42c4fbea79ca048ebee20fc87d
fikepaci <pacifiquemachine@gmail.com>
2020-09-24 12:26:43 +0200

    add 10-print_triangel.c file

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+* print_triangle - prints a triangle using character #.
+*@size: show the size of the triangle.
+*/
+
+void print_triangle(int size)
+{
+int a, b;
+
+if (size < 0)
+{
+for (a = 1; a <= size; a++)
+{
+for (b = size - a; b > 0; b--)
+_putchar(' ');
+
+for (b = 0; b < a; b++)
+_putchar('#');
+
+if (a == size)
+continue;
+
+_putchar('\n');
+}
+}
+_putchar('\n');
+}
