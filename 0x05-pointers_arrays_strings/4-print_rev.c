9b0938e6cb6f636ef65a88c2b7cce204c6cf9ec1
fikepaci <pacifiquemachine@gmail.com>
2020-09-28 19:14:10 +0200

    add 4-print_rev.c file

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+* print_rev - prints a string, in reverse, followed by a new line.
+* @s: string to be printd.
+*/
+
+void print_rev(char *s)
+{
+
+int len = 0, index;
+
+while (s[index++])
+len++;
+
+for (index = len - 1; index >= 0; index--)
+_putchar(s[index]);
+
+_putchar('\n');
+}
