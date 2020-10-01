1500e67178dd0018ea77f912370f6cfcbe5bd937
fikepaci <pacifiquemachine@gmail.com>
2020-10-01 15:35:48 +0200

    change 100-print_number.c file

--- 0x06-pointers_arrays_strings/100-print_number.c
+++ 0x06-pointers_arrays_strings/100-print_number.c
@@ -1,21 +1,52 @@
 #include "holberton.h"
 
 /**
- * print_number - Prints an integer.
- * @n: The integer to be printed.
+ * powten - quick power of ten implementation
+ *
+ * @n: exponent
+ * Return: returns ten to exponent power
+ */
+int powten(int n)
+{
+	int retval;
+
+	retval = 10;
+	while (n > 0)
+	{
+		retval *= 10;
+		n--;
+	}
+	return (retval);
+}
+/**
+ * print_number - prints integers enters as parameters using putchar*
+ * @n: integer to print
+ * Return: void
  */
 void print_number(int n)
 {
-	unsigned int num = n;
+	int sign, power;
 
-	if (n < 0)
+	sign = 1;
+	power = powten(8);
+	if (n > 0)
 	{
-		_putchar('-');
-		num = -num;
+		n = n * -1;
+		sign = sign * -1;
 	}
-
-	if ((num / 10) > 0)
-		print_number(num / 10);
-
-	_putchar((num % 10) + '0');
+	if (n != 0)
+	{
+		while (n / power == 0)
+			power /= 10;
+		if (sign == 1)
+			_putchar('-');
+		while (power >= 1)
+		{
+			_putchar(-(n / power) + '0');
+			n %= power;
+			power /= 10;
+		}
+	}
+	else
+		_putchar('0');
 }
