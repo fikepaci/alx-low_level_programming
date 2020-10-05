88f253e60124f5013e9efac2b902d82c3e8d14ab
fikepaci <pacifiquemachine@gmail.com>
2020-10-05 19:18:28 +0200

    change 8-print_diagsums.c

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -9,12 +9,16 @@
 
 void print_diagsums(int *a, int size)
 {
-int i, j, sum;
+int m;
+int j = size - 1;
+int diagright = 0;
+int diagleft = 0;
 
-for (i = 0, j = 0, sum = 0; i < size; i++, j += size)
-sum += *(a + (i + j));
-printf("%i, ", sum);
-for (i = 0, j = size - 1, sum = 0; i < size; i++, j += size - 2)
-sum += *(a + (i + j));
-printf("%i\n", sum);
+for (m = 0; m < (size * size);
+m = m + (size + 1), j = j + (size - 1))
+{
+diagleft += a[m];
+diagright += a[j];
+}
+printf("%i, %i\n", diagleft, diagright);
 }
