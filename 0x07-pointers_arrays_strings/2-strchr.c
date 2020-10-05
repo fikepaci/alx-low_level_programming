a09f6eef2ed866e5a3176a331a1c6420528d3af7
fikepaci <pacifiquemachine@gmail.com>
2020-10-05 13:56:22 +0200

    add 2-strchr.c file

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+* _strchr - locates a character in a string.
+* @s: string
+* @c: character
+* Return: pointer s value
+*/
+
+char *_strchr(char *s, char c)
+{
+int i;
+
+for (i = 0; s[i] != '\0'; i++)
+{
+if (s[i] == c)
+break;
+}
+if (s[i] == c)
+return (s + i);
+return ('\0');
+}
