3cbffe757b53bd19b4da871493f2690381be3719
fikepaci <pacifiquemachine@gmail.com>
2020-10-07 13:49:00 +0200

    change 1-print_rev_recursion.c file

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,16 +1,14 @@
 #include "holberton.h"
 
 /**
-* _print_rev_recursion - prints a string in reverse.
-* @s: a string to be reversed.
-*/
-
+ * _print_rev_recursion - Prints a string in reverse.
+ * @s: The string to be printed.
+ */
 void _print_rev_recursion(char *s)
 {
-
-if (*s)
-{
-_print_rev_recursion(s + 1);
-_putchar(*s);
-}
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
 }
