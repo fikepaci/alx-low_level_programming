a0473cef9c1491ddfef468376977665c48436ace
fikepaci <pacifiquemachine@gmail.com>
2020-10-07 13:43:00 +0200

    change 7-is_palindrome.c file

--- 0x08-recursion/7-is_palindrome.c
+++ 0x08-recursion/7-is_palindrome.c
@@ -1,27 +1,28 @@
 #include "holberton.h"
 
-int find_strlen(char *s)
+int find_strlen(char *s);
 int check_palindrome(char *s, int len, int index);
 int is_palindrome(char *s);
 
 /**
-* find_strlen - Returns the length of a string.
-* @s: The string to be measured.
-*
-* Return: The length of the string.
-*/
-
+ * find_strlen - Returns the length of a string.
+ * @s: The string to be measured.
+ *
+ * Return: The length of the string.
+ */
 int find_strlen(char *s)
 {
-int len = 0;
+	int len = 0;
 
-if (*(s + len))
-{
-len++;
-len += find_strlen(s + len);
+	if (*(s + len))
+	{
+		len++;
+		len += find_strlen(s + len);
+	}
+
+	return (len);
 }
-return (len);
-}
+
 /**
  * check_palindrome - Checks if a string is a palindrome.
  * @s: The string to be checked.
@@ -33,14 +34,15 @@ return (len);
  */
 int check_palindrome(char *s, int len, int index)
 {
-if (s[index] == s[len / 2])
-return (1);
+	if (s[index] == s[len / 2])
+		return (1);
 
-if (s[index] == s[len - index - 1])
-return (check_palindrome(s, len, index + 1));
+	if (s[index] == s[len - index - 1])
+		return (check_palindrome(s, len, index + 1));
 
-return (0);
+	return (0);
 }
+
 /**
  * is_palindrome - Checks if a string is a palindrome.
  * @s: The string to be checked.
@@ -50,11 +52,11 @@ return (0);
  */
 int is_palindrome(char *s)
 {
-int index = 0;
-int len = find_strlen(s);
+	int index = 0;
+	int len = find_strlen(s);
 
-if (!(*s))
-return (1);
+	if (!(*s))
+		return (1);
 
-return (check_palindrome(s. len, index));
+	return (check_palindrome(s, len, index));
 }
